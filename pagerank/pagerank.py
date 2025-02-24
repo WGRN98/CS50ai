0bbea19851f09f3edb0e0500e3b8f7d467bac850
William Narciso <80212857+WGRN98@users.noreply.github.com>
2025-02-24 20:24:11 +0000

    Complete pagerank.py

--- pagerank/pagerank.py
+++ pagerank/pagerank.py
@@ -57,7 +57,28 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
-    raise NotImplementedError
+    # Initialize probability distribution dictionary and set number of pages and links in page
+    prob_dist = {page_name: 0 for page_name in corpus}
+    num_pages = len(corpus)
+    num_links = len(corpus[page])
+
+    # If page has no links return equal probability for corpus
+    if len(corpus[page]) == 0:
+        for page_name in prob_dist:
+            prob_dist[page_name] = 1 / num_pages
+        return prob_dist
+
+    # Probability of picking page at random and picking a link at random from a page
+    random_prob = (1 - damping_factor) / num_pages
+    link_prob = damping_factor / num_links
+
+    # Adding each probability to the distribution
+    for page_name in prob_dist:
+        prob_dist[page_name] += random_prob
+        if page_name in corpus[page]:
+            prob_dist[page_name] += link_prob
+
+    return prob_dist
 
 
 def sample_pagerank(corpus, damping_factor, n):
@@ -69,7 +90,27 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    # Storing page ranks and choosing a page at random
+    page_rank = {}
+    next_page = random.choice(list(corpus))
+
+    # Iterate over the rest of the samples
+    for i in range(n-1):
+        # Create a transition model each time with the probability of each page and choose the next page at random considering transition model
+        model = transition_model(corpus, next_page, damping_factor)
+        next_page = random.choices(list(model), weights=model.values(), k=1).pop()
+
+        # Fill page rank with the number of times page is visited
+        if next_page in page_rank:
+            page_rank[next_page] += 1
+        else:
+            page_rank[next_page] = 1
+
+    # Normalize the number of visits
+    for page in page_rank:
+        page_rank[page] = page_rank[page] / n
+
+    return page_rank
 
 
 def iterate_pagerank(corpus, damping_factor):
@@ -81,7 +122,48 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    num_pages = len(corpus)
+    random_prob = (1 - damping_factor) / num_pages
+    init_prob = 1 / num_pages
+    iteration = 0
+
+    # Initial page rank
+    page_rank = {page_name: init_prob for page_name in corpus}
+    new_rank = {page_name: None for page_name in corpus}
+    max_change = init_prob
+
+    # Iterate until change < 0.001
+    while max_change > 0.001:
+        iteration += 1
+        max_change = 0
+
+        for page_name in corpus:
+            choice_prob = 0
+            for other_page in corpus:
+                # If page has no links pick randomly from corpus
+                if len(corpus[other_page]) == 0:
+                    choice_prob += page_rank[other_page] * init_prob
+                # If page has links pick randomly from all links
+                elif page_name in corpus[other_page]:
+                    choice_prob += page_rank[other_page] / len(corpus[other_page])
+            # Calculating new ranks of page
+            rank = random_prob + (damping_factor * choice_prob)
+            new_rank[page_name] = rank
+
+        # Normaliza new page ranks, like in sample_pagerank
+        norm_factor = sum(new_rank.values())
+        new_rank = {page: (rank / norm_factor) for page, rank in new_rank.items()}
+
+        # Find the max change in page ranks
+        for page_name in corpus:
+            change = abs(page_rank[page_name] - new_rank[page_name])
+            if change > max_change:
+                max_change = change
+
+        # Update the page ranks with the new ranks
+        page_rank = new_rank.copy()
+
+    return page_rank
 
 
 if __name__ == "__main__":
